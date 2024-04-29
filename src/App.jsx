439302fb6cf0b732b057fe2b4465c6b69d295dc7
marischi <marileneifsp@gmail.com>
2024-04-28 21:07:34 -0300

    FotoAmpliada

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import FotoAmpliada from './components/FotoAmpliada';
+import FotoList from './components/FotoList';
+import SearchBar from './components/SearchBar';
+
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [categoria, setCategoria] = useState('');
+  const [fotos, setFotos] = useState([]);
+  const [fotoAmpliada, setFotoAmpliada] = useState(null);
+  const [activateSearch, setActivateSearch] = useState(false);
+
+  const fetchData = async ({ query, categoria }) => {
+    const apiKey = import.meta.env.VITE_UNSPLASH_API_KEY;
+    const response = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: apiKey,
+        count: 12,
+      },
+    });
+
+    setFotos(response.data);
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchData(query, categoria);
+  }, []);
+
+  return (
+    <div className='container'>
+      <SearchBar />
+      <FotoList fotos={fotos} setFotoAmpliada={setFotoAmpliada} />
+      {fotoAmpliada && (
+        <FotoAmpliada foto={fotoAmpliada} setFotoAmpliada={setFotoAmpliada} />
+      )}
+    </div>
+  );
+}
+
+export default App;
